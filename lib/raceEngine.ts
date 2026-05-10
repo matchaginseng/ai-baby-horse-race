bdbfd9dff655435d4e7e07011da0b7e26cc06dc9
VICKI XU <vickixu20@gmail.com>
2026-05-10 16:59:31 -0400

    param

--- lib/raceEngine.ts
+++ lib/raceEngine.ts
@@ -58,7 +58,7 @@ const TICK_MS       = 16;
 const SLIP_CHANCE   = 0.0018;
 const SLIP_FRAMES   = 50;
 const SLIP_SPEED    = 2.5;
-const CAREER_CHANCE = 0.0004;
+const CAREER_CHANCE = 0.0003;
 
 function seededRand(seed: number) {
   let s = seed;
